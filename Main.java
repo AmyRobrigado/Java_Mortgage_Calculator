2020b090395155de7bead0467d80fd9c17b85959
Amy Robrigado <amyantanod@gmail.com>
2023-01-24 21:07:41 -0500

    Finished calculator

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner ui = new Scanner(System.in); // this is to grab user data
+
+        System.out.println("Enter loan amount: ");
+        double principal = ui.nextDouble();
+        System.out.println("Loan amount: " + NumberFormat.getCurrencyInstance().format(principal));
+
+        System.out.println("Enter your down payment: ");
+        double downPayment = ui.nextDouble();
+        System.out.println("Down Payment Amount:" + downPayment);
+
+        System.out.println("Enter interest rate:");
+        double interest = (ui.nextDouble()/100)/12;
+        System.out.println("Interest is :" + interest);
+
+        System.out.println("Loan term in years:");
+        double months = ui.nextDouble()*12;
+        System.out.println("Loan term in months:" + months);
+
+        double value = Math.pow(1 + interest , months);
+        double payment = (principal - downPayment) * ((interest * value)/(value - 1));
+
+        System.out.println("Your monthly payment is:" + NumberFormat.getCurrencyInstance().format(payment));
+        System.out.println("Your bi-weekly payment is:" + NumberFormat.getCurrencyInstance().format(payment/2));
+
+        ui.close();
+    }
+}
